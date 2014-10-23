ae03287213a836fe99e6d8343c192034b06650c2
pedropazello <pedropazello.rj@gmail.com>
2014-10-23 16:03:26 -0200

    muuuuuuuch better

--- java/Problem3.java
+++ java/Problem3.java
@@ -4,48 +4,18 @@ import java.util.*;
 
 class Problem3 {
 
-    public static boolean isPrime(int n) {
-        int res = 0;
-        int err = 0;
-        for(int x = 2; x < n; x++) {
-           res = n % x;
-           if(res == 0)
-               err++;
-        }
-        if(err > 0)
-            return false;
-        else
-            return true;
-    }
-
-    public static List<Integer> generatePrimes(int amnt) {
-        List<Integer> primes = new ArrayList<Integer>();
-        for(int x = 1; x < amnt; x++) {
-            if(isPrime(x))
-                primes.add(x);
-        }
-        return primes;
-    }
-
     public static int largestPrime(Long num) {
-        List<Integer> primes = generatePrimes(100000);
-        List<Integer> primesUsed = new ArrayList<Integer>();
-        int x = 0;
+        int large = 0;
+        int x = 1;
         Long quo = 0L;
         while(quo != 1) {
-            Long res = (Long) (num % primes.get(x));
+            Long res = (Long) (num % x);
             if(res == 0L) {
-                quo = (Long) (num / primes.get(x));
-                num = (Long) (num / primes.get(x));
-                primesUsed.add(primes.get(x));
+                quo = (Long) (num / x);
+                num = (Long) (num / x);
+                large = x;
             }
             x++;
-        }
-
-        int large = 0;
-        for(int y = 0; y < primesUsed.size(); y++) {
-            if(primesUsed.get(y) > large)
-                large = primesUsed.get(y);
         }
         return large;
     }
